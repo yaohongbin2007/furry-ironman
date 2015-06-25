ae3840c4b40b3f21717eedd8871e6018aa750b6a
yao <yao_seu@163.com>
2015-06-25 22:27:48 +0800

    added a person class

---
+++ TestGit/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  TestGit
+//
+//  Created by bme on 15/6/25.
+//  Copyright (c) 2015年 bme. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ TestGit/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  TestGit
+//
+//  Created by bme on 15/6/25.
+//  Copyright (c) 2015年 bme. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
